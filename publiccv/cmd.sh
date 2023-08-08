68c3827126c0229ec083590354b143302e44afbb
Artemy Kolchinsky <artemyk@gmail.com>
2023-08-07 19:58:37 -0700

    Update

--- publiccv/cmd.sh
+++ publiccv/cmd.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 pandoc pubs.tex -o ../_includes/pubs.html 
+pandoc popular.tex -o ../_includes/popular.html 
 pandoc preprints.tex -o ../_includes/preprints.html 
 pdflatex cv.tex && cp cv.pdf ../assets/pdf 
